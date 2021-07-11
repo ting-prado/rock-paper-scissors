be931d218dd83a0ca26206b067eb22df847ff526
Ethel <ethel.prado1998@gmail.com>
2021-07-11 14:47:56 +0800

    Single round function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,6 +10,34 @@ function playerPlay() {
     return playerChoice.toLowerCase();
 }
 
+function singleRound(playerSelection, computerSelection) {
+    if ((playerSelection === 'rock' && computerSelection === 'Rock') || 
+        (playerSelection === 'paper' && computerSelection === 'Paper') ||
+        (playerSelection === 'scissors' && computerSelection === 'Scissors')
+    ) {
+        return "It's a tie!";
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'Paper') {
+        return "You Lose! Paper beats Rock.";
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'Rock') {
+        return "You Win! Paper beats Rock.";
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'Scissors') {
+        return "You Win! Rock beats Scissors.";
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'Rock') {
+        return "You Lose! Rock beats Scissors.";
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'Scissors') {
+        return "You Lose! Scissors beats Paper.";
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
+        return "You Win! Scissors beats Paper.";
+    }
+    else return "Invalid input.";
+}
+
 function game(){
     let computerScore = 0,
         playerScore = 0,
