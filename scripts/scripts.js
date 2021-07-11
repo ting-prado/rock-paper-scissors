54a23cebf3143aa5b6cad5580f10b12cc7b77019
Ethel <ethel.prado1998@gmail.com>
2021-07-11 14:19:41 +0800

    Computer and player play function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,17 @@
 console.log('Welcome to rock, paper, scissors game');
+
+function computerPlay(){
+    let computerChoice = ['Rock', 'Paper', 'Scissors'];
+    return computerChoice[Math.floor(Math.random()*3)];
+}
+
+function playerPlay() {
+    let playerChoice = prompt('Rock, paper or scissors? ');
+    return playerChoice.toLowerCase();
+}
+
+function game(){
+    let computerScore = 0,
+        playerScore = 0,
+        round = 5;
+}
